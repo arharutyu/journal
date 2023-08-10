1c09160a06227a941cc35ec203bd15d37f85d9c5
arharuty <13937@coderacademy.edu.au>
2023-08-10 13:09:17 +1000

    test app component

---
+++ src/components/App.test.jsx
@@ -0,0 +1,16 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+
+describe('App Component', () => {
+  it('Renders the Home component', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    )
+
+    expect(screen.getByRole('heading', { level:2 })).toHaveTextContent('Journal Entries')
+  })
+})
