e27d109a2ca01caf29e5a659abcd7b6ff0d0745f
arharuty <13937@coderacademy.edu.au>
2023-08-17 13:12:01 +1000

    Added reducer and setEntries + addEntry actions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import Home from './Home'
 import CategorySelection from './CategorySelection'
 import NewEntry from './NewEntry'
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useNavigate, useParams } from 'react-router-dom'
 import NavBar from './NavBar'
 import ShowEntry from './ShowEntry'
-import { useParams } from 'react-router-dom'
 
 // const seedEntries = [
 //   {category: 'Food', content: 'Is energy'},
@@ -13,16 +12,46 @@ import { useParams } from 'react-router-dom'
 //   {category: 'Gaming', content: 'Animal Crossing is a game.'}
 // ]
 
+function reducer(currentState, action) {
+  switch(action.type) {
+    case 'setEntries':
+      return {
+      // deconstruct into key value pairs (entries & categories keys)
+      ...currentState,
+      // update entries value with entries from action
+      entries: action.entries
+    }
+    case 'addEntry':
+      return {
+        ...currentState,
+        entries: [...currentState.entries, action.entry]
+      }
+    default:
+      return currentState
+  }
+}
+
+const initialState = {
+  entries: [],
+  categories: []
+}
+
 const App = () => {
   const nav = useNavigate()
-  const [entries, setEntries] = useState([])
+  // const [entries, setEntries] = useState([])
+
+  const [store, dispatch] = useReducer(reducer, initialState)
+  const {entries} = store
 
   useEffect(() => {
     // closure/ IIFE - declare anon fx, immediately call
       (async () => {
       const res = await fetch(`${import.meta.env.VITE_API_HOST}/entries`)
       const data = await res.json()
-      setEntries(data)
+      dispatch({
+        type: 'setEntries',
+        entries: data,
+      })
       })()
   }, [])
 
@@ -43,7 +72,11 @@ const App = () => {
       body: JSON.stringify({ category, content })
     })
 
-    setEntries([...entries, await returnedEntry.json()])
+    dispatch({
+      type: 'addEntry',
+      entry: await returnedEntry.json()
+    })
+
     nav(`/entry/${id}`)
   }
 
